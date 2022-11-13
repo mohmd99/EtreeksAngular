4f85f037b6185fc414891fc4bf1704d37c0e5087
mohammed <mohammedsaadeh473@gmail.com>
2022-11-13 22:38:06 +0300

    dd

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,11 +8,31 @@ import { FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-rememberMe: boolean=false;
-  ngOnInit(): void {
-    
-  }
+  rememberMe:boolean=false;
   email = new FormControl('',[Validators.required,Validators.email]);
   password = new FormControl('',[Validators.required,Validators.minLength(8)]);
 
+  ngOnInit(): void {
+    this.rememberMe = false;
+
+  }
+  onSubmit() {
+
+
+
+  //     localStorage.setItem('email', this.email);
+  //     localStorage.setItem('password',  this.password);
+  //     // Save value to local storage
+  //     if(this.rememberMe) {
+  //       localStorage.setItem('rememberMe', 'yes')
+  //     }
+  //     this.toastrService.success('Successfully Login!');
+  //     this.router.navigate(['/home']);
+  //   },
+  //   (err) => {
+
+  //   });
+  // }
+
+}
 }
