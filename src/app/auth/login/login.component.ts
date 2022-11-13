188f5b482bef0457a63349bd594dfa0c2f15110d
mohammed <mohammedsaadeh473@gmail.com>
2022-11-13 23:12:05 +0300

    fdsfdsa

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -8,28 +8,33 @@ import { FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-  rememberMe:boolean=false;
-  email = new FormControl('',[Validators.required,Validators.email]);
-  password = new FormControl('',[Validators.required,Validators.minLength(8)]);
-  data=new FormControl();
+  rememberMe: boolean = false;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  data = new FormControl();
   ngOnInit(): void {
     this.rememberMe = false;
     this.AutoLogin();
 
   }
-  Remember(ev:any){
-    if(ev.target.checked){
-      localStorage.setItem('email',String(this.email));
-      localStorage.setItem('password',String(this.password));
+  Remember(ev: any) {
+    if (ev.target.checked) {
+      localStorage.setItem('email', String(this.email.value));
+      localStorage.setItem('password', String(this.password.value));
     }
-  } 
-  AutoLogin(){
- this.email=new FormControl(localStorage.getItem('email'));
- 
-  this.password=new FormControl( localStorage.getItem('password'));  
+  }
+  AutoLogin() {
+    if (localStorage.getItem('email') != '' && localStorage.getItem('password') != '') {
+      this.email = new FormControl(localStorage.getItem('email'), [Validators.required, Validators.email]);
+
+      this.password = new FormControl(localStorage.getItem('password'), [Validators.required, Validators.minLength(8)]);
+    }
+    console.log(this.email);
+
+
   }
 
-    
+
   /*{
 
 
