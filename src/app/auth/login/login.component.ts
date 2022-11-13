ee1c0143a151d9d9a5893d5e4c73a46949c37308
ABDALLAH BATAYNEH <ambatayneh197@cit.just.edu.jo>
2022-11-13 21:55:32 +0300

    addd

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -11,28 +11,42 @@ export class LoginComponent implements OnInit {
   rememberMe:boolean=false;
   email = new FormControl('',[Validators.required,Validators.email]);
   password = new FormControl('',[Validators.required,Validators.minLength(8)]);
-
+  data=new FormControl();
   ngOnInit(): void {
     this.rememberMe = false;
+    this.AutoLogin();
 
   }
-  onSubmit() {
+  Remember(ev:any){
+    if(ev.target.checked){
+      localStorage.setItem('email',String(this.email));
+      localStorage.setItem('password',String(this.password));
+    }
+  } 
+  AutoLogin(){
+ this.email=new FormControl(localStorage.getItem('email'));
+ 
+  this.password=new FormControl( localStorage.getItem('password'));  
+  }
+
+    
+  /*{
 
 
 
-  //     localStorage.setItem('email', this.email);
-  //     localStorage.setItem('password',  this.password);
-  //     // Save value to local storage
-  //     if(this.rememberMe) {
-  //       localStorage.setItem('rememberMe', 'yes')
-  //     }
-  //     this.toastrService.success('Successfully Login!');
-  //     this.router.navigate(['/home']);
-  //   },
-  //   (err) => {
+      localStorage.setItem('email', this.email);
+      localStorage.setItem('password',  this.password);
+     // Save value to local storage
+     if(this.rememberMe) {
+        localStorage.setItem('rememberMe', 'yes')
+      }
+      this.toastrService.success('Successfully Login!');
+      this.router.navigate(['/home']);
+   },
+    (err) => {
 
-  //   });
-  // }
+    });
+   }*/
 
 }
-}
+
