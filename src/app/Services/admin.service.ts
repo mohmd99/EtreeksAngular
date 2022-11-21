5cdbff36b6596e3b3ab77684544b3a13e6da84ae
mohammed <mohammedsaadeh473@gmail.com>
2022-11-22 02:40:13 +0300

    comment

--- src/app/Services/admin.service.ts
+++ src/app/Services/admin.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+coursesinfo:any=[{}]
+  constructor( private toaster:ToastrService,private spinner:NgxSpinnerService,private http:HttpClient) { }
 
-  constructor() { }
+  getcoursewithcategory(){
+    this.spinner.show();
+    this.http.get("https://localhost:44343/api/course/getcoursewithcategory").subscribe((res:any)=>{
+      this.coursesinfo=res;
+      this.spinner.hide();
+      console.log(res);
+
+      this.toaster.success("retreived");
+
+    },err=>{
+      this.spinner.hide();
+      this.toaster.error(err.message,err.status);
+    });
+  }
 }
