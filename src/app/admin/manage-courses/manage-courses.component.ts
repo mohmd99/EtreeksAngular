033f884f57610bcdd1b242b46b22d028c383d65c
Designer <Designer@DESKTOP-A7M9MSC>
2022-11-22 01:20:35 +0300

    manageCourses

--- src/app/admin/manage-courses/manage-courses.component.ts
+++ src/app/admin/manage-courses/manage-courses.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from 'src/app/Services/general.service';
 
 @Component({
   selector: 'app-manage-courses',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCoursesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public generalServices:GeneralService) { }
+
 
   ngOnInit(): void {
+    this.generalServices.GetAllCourses();
   }
 
 }
