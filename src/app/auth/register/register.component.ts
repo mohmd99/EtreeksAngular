7c90d266b0124eec49ca68b492afd6ef1c341ea1
ABDALLAH BATAYNEH <ambatayneh197@cit.just.edu.jo>
2022-11-22 01:24:55 +0300

    addd

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+  firstname=new FormControl('',[Validators.required]);
+  lastname=new FormControl('',[Validators.required]);
+  phonenumber=new FormControl('+962',[Validators.required,Validators.minLength(9),Validators.maxLength(9)]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(8)]);
 
   ngOnInit(): void {
   }
