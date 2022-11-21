46349899c643272402386d4e33bd9fc0ca11a058
Designer <Designer@DESKTOP-A7M9MSC>
2022-11-21 22:07:25 +0300

    admin routnig

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 const routes: Routes = [
-  
+  {path:"",component:AdminPanelComponent
+}
 ];
 
 @NgModule({
