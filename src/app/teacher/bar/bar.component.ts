4a00c966dfb258528264950a43eea7331a51a0b7
mohammed <mohammedsaadeh473@gmail.com>
2022-12-03 13:33:58 +0300

    side nav trainer

--- src/app/teacher/bar/bar.component.ts
+++ src/app/teacher/bar/bar.component.ts
@@ -11,45 +11,13 @@ export class BarComponent implements OnInit {
   @Input() first_Name:string="";
   @Input() last_Name:string="";
   @Input() image:string|undefined;
+  @Input() color:string="";
   constructor( private router :Router,public teacherService:TeacherService) { }
 
   ngOnInit(): void {
   }
-  Colors:any[]=[
-  "#8B7E74",
-"#344D67",
-"#8B7E74",
-"#C7BCA1",
-"#497174",
-"#3A8891",
-"#6D9886",
-"#8D9EFF",
-"#9BA17B",
-"#5DA7DB",
-"#9E7676",
-"#98A8F8",
-"#251B37",
-"#1C6758",
-"#704F4F",
-"#FF731D",
-"#AC4425",
-"#A10035",
-"#D1512D",
-"#18978F",
-"#B20600",
-"#00092C",
-"#D9534F",
-"#8A8635",
-"#630000",
-"#334756",
-"#6C4A4A",
-"#105652",
-"#694E4E"
-  ]
-  default={
-    color:this.Colors[this.first_Name.toUpperCase().charCodeAt(0)-97],
 
-  }
+
 
   GoToHome(){
     this.router.navigate(['teacher/home'])
--- src/app/teacher/home/home.component.ts
+++ src/app/teacher/home/home.component.ts
@@ -1,3 +1,5 @@
+import { AdminService } from './../../Services/admin.service';
+import { AdminModule } from './../../admin/admin.module';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/Services/auth.service';
@@ -10,54 +12,16 @@ import { TeacherService } from 'src/app/Services/teacher.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  name:any="";
-  @Input() first_Name:string="";
-  @Input() last_Name:string="";
-  @Input() image:string|undefined;
-  @Input() color:string="";
-  constructor(public teacherService:TeacherService,public authService:AuthService ,private router:Router,public generalService:GeneralService) { }
 
-  ngOnInit(): void { 
-    this.generalService.GetHomeInfo();
-    this.teacherService.getuserbyid(this.authService.data.ID);
+
+  constructor(public adminService:AdminService,public teacherService:TeacherService,public authService:AuthService ,private router:Router,public generalService:GeneralService) { }
+
+  ngOnInit(): void {
+
+    this.adminService.getuserbyid(this.authService.data.ID);
     this.teacherService.getloginuserbyid(this.authService.data.ID);
   }
-  myName:string=this.teacherService.userbyid.first_Name;
-  Colors:any[]=[
-    "#8B7E74",
-  "#344D67",
-  "#8B7E74",
-  "#C7BCA1",
-  "#497174",
-  "#3A8891",
-  "#6D9886",
-  "#8D9EFF",
-  "#9BA17B",
-  "#5DA7DB",
-  "#9E7676",
-  "#98A8F8",
-  "#251B37",
-  "#1C6758",
-  "#704F4F",
-  "#FF731D",
-  "#AC4425",
-  "#A10035",
-  "#D1512D",
-  "#18978F",
-  "#B20600",
-  "#00092C",
-  "#D9534F",
-  "#8A8635",
-  "#630000",
-  "#334756",
-  "#6C4A4A",
-  "#105652",
-  "#694E4E"
-    ]
-    default={
-      color:this.Colors[this.first_Name.toUpperCase().charCodeAt(0)-97],
-  
-    }
-  
-    
+
+
+
 }
