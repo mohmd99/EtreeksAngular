b312e1c0c45f3474cb93a6d0667743c387e544e1
mohammed <mohammedsaadeh473@gmail.com>
2022-12-04 20:42:11 +0300

    verify3

--- src/app/auth/check-verify/check-verify.component.ts
+++ src/app/auth/check-verify/check-verify.component.ts
@@ -9,17 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class CheckVerifyComponent implements OnInit {
 
   constructor(private authService:AuthService) { }
-
+ disable:boolean=false;
   ngOnInit(): void {
     this.authService.getloginbyid(this.authService.Ids[0].value);
+    setTimeout(() => {
+      if(this.authService.loginbyid.verify_Code==0){
+      this.disable=true;
+      }
+      }, 35000);
   }
-  disable:boolean=false;
+
   checkverify(ev:any){
 this.authService.checkVerify(ev.target.value,this.authService.Ids[0].value);
-setTimeout(() => {
-if(this.authService.loginbyid.verify_Code==0){
-this.disable=true;
-}
-}, 30000);
+
   }
 }
